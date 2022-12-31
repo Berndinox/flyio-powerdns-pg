2bcf7ef8481c4baa5870c12a214dd0b2e40f55af
berndinox <berndinox@gmail.com>
2022-12-31 10:59:29 +0100

    Init

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,20 @@
 
 
 sed -i 's|.*gpgsql-host=.*|'gpgsql-host=$PDNSCONF_GPGSQL_HOST'|g' /etc/pdns/pdns.conf
+sed -i 's|.*gpgsql-port=.*|'gpgsql-port=$PDNSCONF_GPGSQL_PORT'|g' /etc/pdns/pdns.conf
 sed -i 's|.*gpgsql-dbname=.*|'gpgsql-dbname=$PDNSCONF_GPGSQL_DBNAME'|g' /etc/pdns/pdns.conf
 sed -i 's|.*gpgsql-user=.*|'gpgsql-user=$PDNSCONF_GPGSQL_USER'|g' /etc/pdns/pdns.conf
 sed -i 's|.*gpgsql-password=.*|'gpgsql-password=$PDNSCONF_GPGSQL_PASSWORD'|g' /etc/pdns/pdns.conf
 sed -i 's|.*api-key=.*|'api-key=$PDNSCONF_API_KEY'|g' /etc/pdns/pdns.conf
 
-if [ "${PDNSCONF_SERVER_TYPE}" == "master" ]; then
-  sed -i 's|.*sedreplacementcode.*|'master=yes\ \#Or:\ slave=yes\ -sedreplacementcode'|g' /etc/pdns/pdns.conf
-elif [ "${PDNSCONF_SERVER_TYPE}" == "slave" ]; then
-  sed -i 's|.*sedreplacementcode.*|'slave=yes\ \#Or:\ master=yes\ -sedreplacementcode'|g' /etc/pdns/pdns.conf
+
+
+if [ "${PDNSCONF_SERVER_MODE}" == "master" ]; then
+  sed -i 's|.*sedreplacementcode.*|'primary=yes\ \#Or:\ secondary=yes\ -sedreplacementcode'|g' /etc/pdns/pdns.conf
+elif [ "${PDNSCONF_SERVER_MODE}" == "slave" ]; then
+  sed -i 's|.*sedreplacementcode.*|'secondary=yes\ \#Or:\ primary=yes\ -sedreplacementcode'|g' /etc/pdns/pdns.conf
 else
-  sed -i 's|.*sedreplacementcode.*|'master=yes\ \#Or:\ slave=yes\ -sedreplacementcode'|g' /etc/pdns/pdns.conf
+  sed -i 's|.*sedreplacementcode.*|'primary=yes\ \#Or:\ secondary=yes\ -sedreplacementcode'|g' /etc/pdns/pdns.conf
 fi
 
 echo "Lets Fly our PowerDNS!"
